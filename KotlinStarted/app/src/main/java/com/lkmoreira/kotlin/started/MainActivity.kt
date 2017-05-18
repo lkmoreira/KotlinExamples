cf6a6590ec71db0a40c24aa4f3339c6812d059e4
Lucas Moreira <lucas.barbara@engdb.com.br>
2017-05-18 17:27:47 -0300

    Created layout and activity of first screen

--- KotlinStarted/app/src/main/java/com/lkmoreira/kotlin/started/MainActivity.kt
+++ KotlinStarted/app/src/main/java/com/lkmoreira/kotlin/started/MainActivity.kt
@@ -2,11 +2,17 @@ package com.lkmoreira.kotlin.started
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val openNext = findViewById(R.id.open_next) as Button
+
+        openNext.setOnClickListener { Toast.makeText(this, "Next Activity", Toast.LENGTH_LONG).show() }
     }
 }
