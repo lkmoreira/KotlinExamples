6ffdb3ad9aa498d29b142acb9ecc64c5f3ed510b
Lucas Moreira <lucas.barbara@engdb.com.br>
2017-05-19 17:15:25 -0300

    Created second activity

---
+++ KotlinStarted/app/src/main/java/com/lkmoreira/kotlin/started/business/SecondActivity.kt
@@ -0,0 +1,14 @@
+package com.lkmoreira.kotlin.started.business
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.lkmoreira.kotlin.started.R
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
